6519eacc4195ff1eed09a2b859902a4484d27f4f
ekatrif <katiapervak@gmail.com>
2023-02-07 12:32:05 +0100

    feat: task in done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,35 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.result=0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
+        //Угадываем результат рандомом из ряда между min и max
+        this.result = this.min + Math.round((this.max - this.min) / 2);
+        return this.result;
+        // (this.result>this.number) ? this.lower(this.result) : this.greater(this.result);
+        // return this.result;
+        //если результат > загаданного, вызываем метод lower(), иначе greater
+        // если угадали return result
+        
 
     }
 
     lower() {
-
+        //новый max - result
+        //min прежний
+        this.max = this.result;
     }
 
     greater() {
-
+        //новый min - result
+        //max прежний
+        this.min = this.result;
     }
 }
 
